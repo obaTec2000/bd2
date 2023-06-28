2cda168cde2ba437c70efe73fb9c1d66eb4338e7
Adedeji Oluwaobi <oluwatobiisaiah0409@gmail.com>
2023-06-28 12:21:42 +0100

    New Project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter,Outfit } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   )
 }
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+// check if user is logged in first, if user is logged in then redirect to hone page
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    </main>
+  )
+}
